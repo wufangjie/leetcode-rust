1c9e4912a9b2391e0e9a88c90014b69087a43323
wfj <wufangjie1223@126.com>
2021-11-20 01:15:48 +0800

    use Vec, VecDeque instead of stack, queue

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-// mod s0704_binary_search;
+use leetcode_rust::dbgt;
+
+mod s0704_binary_search;
 // mod s0705_design_hashset;
 // mod s0706_design_hashmap;
 
@@ -9,5 +11,6 @@ mod s0754_reach_a_number;
 mod s0756_pyramid_transition_matrix;
 mod s0757_set_intersection_size_at_least_two;
 
+mod s0762_prime_number_of_set_bits_in_binary_representation;
 
 fn main() {}
