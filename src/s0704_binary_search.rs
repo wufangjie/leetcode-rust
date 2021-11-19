954c31245eca5e758ad3c8479b762f7f92446b5f
wfj <wufangjie1223@126.com>
2021-11-20 01:06:14 +0800

    remove comment

--- src/s0704_binary_search.rs
+++ src/s0704_binary_search.rs
@@ -17,7 +17,7 @@ impl Solution {
 }
 
 #[test]
-fn test() {
+fn test_704() {
     assert_eq!(Solution::search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
     assert_eq!(Solution::search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
 }
