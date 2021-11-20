62d25be19714f7987449a22d6a05f749842bf354
wfj <wufangjie1223@126.com>
2021-11-20 21:17:24 +0800

    use vec instead of hashset, and not record result

--- src/s0753_cracking_the_safe.rs
+++ src/s0753_cracking_the_safe.rs
@@ -1,41 +1,37 @@
-use leetcode_rust::utils::Stack;
-use std::collections::HashSet;
-
 struct Solution;
 
 impl Solution {
     pub fn crack_safe(n: i32, k: i32) -> String {
-        let mut s = String::from("");
-        let k = k as u8;
+        let mut s = format!("{:>0width$}", 0, width = n as usize - 1);
         if n == 1 {
-            for i in 48u8..(48u8 + k) {
+            for i in 49u8..(48u8 + k as u8) {
                 s.push(i as char);
             }
             return s;
         }
-        let mut visited = HashSet::new();
-        visited.insert(0u16);
-
-        let total = (k as usize).pow(n as u32);
-        let base = 10u16.pow(n as u32 - 1);
-        let mut stack: Stack<(u16, HashSet<u16>, String)> = Stack::new();
-        stack.push((0u16, visited, format!("{:>0width$}", 0, width = n as usize)));
+        let k = k as usize;
+        let total = k.pow(n as u32) as u16;
+        let mut visited = vec![0u16; total as usize];
+        visited[0] = 1;
+        let base = k.pow(n as u32 - 1) as u16;
+        let mut stack = vec![(0u16, 1u16, visited)];
 
         loop {
-            let (pre, visited, ret) = stack.pop().unwrap();
-            if visited.len() == total {
-                return ret;
+            let (pre, count, visited) = stack.pop().unwrap();
+            if count == total {
+                let mut temp: Vec<(usize, u16)> = visited.into_iter().enumerate().collect();
+                temp.sort_by(|a, b| a.1.cmp(&b.1));
+                for (i, _) in temp {
+                    s.push(((i % k) as u8 + 48) as char);
+                }
+                return s;
             }
-            let pre10 = pre * 10;
-
-            for i in 0..k {
-                let key = pre10 + i as u16;
-                if !visited.contains(&key) {
+            let pre_k = pre as usize * k;
+            for i in pre_k..pre_k + k {
+                if visited[i] == 0 { // Euler's path exist!
                     let mut v2 = visited.clone();
-                    v2.insert(key);
-                    let mut r2 = ret.clone();
-                    r2.push((i + 48) as char);
-                    stack.push((key % base, v2, r2));
+                    v2[i] = count + 1;
+                    stack.push(((i as u16) % base, count + 1, v2));
                 }
             }
         }
@@ -43,7 +39,7 @@ impl Solution {
 }
 
 #[test]
-fn test() {
+fn test_753() {
     println!("{:?}", Solution::crack_safe(1, 5));
     println!("{:#?}", Solution::crack_safe(2, 2));
     println!("{:#?}", Solution::crack_safe(3, 4));
